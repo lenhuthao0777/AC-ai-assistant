af0acf7b69694f59c12eb9cbd1713711af2f57fc
LeNhutHao <lenhuthao0777@gmail.com>
2024-01-11 19:08:07 +0700

    format code

--- src/app/(main)/review/[owner]/_component/content.tsx
+++ src/app/(main)/review/[owner]/_component/content.tsx
@@ -1,22 +1,10 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-import parse, { Change } from 'parse-diff';
+import { Change } from 'parse-diff';
 import Diff from './diff';
-import { useAppSelector } from '@/redux/hook';
 import { File } from 'gitdiff-parser';
 import { useMemo } from 'react';
 import { parseDiff } from 'react-diff-view';
-
-enum DiffMethod {
-  CHARS = 'diffChars',
-  WORDS = 'diffWords',
-  WORDS_WITH_SPACE = 'diffWordsWithSpace',
-  LINES = 'diffLines',
-  TRIMMED_LINES = 'diffTrimmedLines',
-  SENTENCES = 'diffSentences',
-  CSS = 'diffCss',
-}
 
 export type FileInfoWithDiff = {
   changeLine: { content: string; changes: Change[] }[];
@@ -27,10 +15,6 @@ export type FileInfoWithDiff = {
 };
 
 export default function Content({ diff }: { diff: any }) {
-  // const transform = (diff:any) => {
-
-  // };
-
   const fileList = useMemo(() => {
     let result;
 
